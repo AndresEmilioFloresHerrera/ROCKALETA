605d41733cb1cf3adbd65752d9c1cc6494220645
Emilio Flores Herrera <Andres.E.Flores.Herrera-1@ou.edu>
2020-01-17 01:38:46 -0600

    2.1 IN PROCCES

---
+++ JJJ/2.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Grade1 = 0, Grade2 = 0, Grade3 = 0, Percentage;
+            for (int i = 0; i < 3; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine("Please enter your first Grade: ");
+                    Grade1 = Convert.ToDouble(Console.ReadLine());
+
+                }
+                else if (i == 1)
+                {
+                    Console.WriteLine("Please enter your second Grade: ");
+                    Grade2 = Convert.ToDouble(Console.ReadLine());
+                }
+                else if (i == 2)
+                {
+                    Console.WriteLine("Please enter your third Grade: ");
+                    Grade3 = Convert.ToDouble(Console.ReadLine());
+                }
+
+            }
+
+            Percentage = (Grade1 + Grade2 + Grade3) / 3;
+            Console.WriteLine($"the percentage of your 3 grades is {Percentage}% out of a 100%");
+            Console.ReadKey();
+        }
+    }
+}
