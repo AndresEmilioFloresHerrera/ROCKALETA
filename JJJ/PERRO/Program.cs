7da9f695c2c4a1e730e4416fdf31472c70274c21
Emilio Flores Herrera <Andres.E.Flores.Herrera-1@ou.edu>
2020-01-16 23:06:02 -0600

    perro added

---
+++ JJJ/PERRO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PERRO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string Hola;
+            Console.WriteLine("Please enter a number");
+            Hola = Console.ReadLine();
+
+            Console.WriteLine($"Hello dear {Hola}");
+
+
+        }
+    }
+}
