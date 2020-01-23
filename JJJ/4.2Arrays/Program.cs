27b8abe432852950f8bcd4db1b8716184273be63
Emilio Flores Herrera <Andres.E.Flores.Herrera-1@ou.edu>
2020-01-22 23:45:49 -0600

    YA CASI

---
+++ JJJ/4.2Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _4._2Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] lowercaseLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] uppercaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+
+
+            //Create a console application that will print out your first name and last name, with correct
+            //punctuation(e.g.upper case characters where needed, like the first letter of each name).  To get the characters,
+            //create two arrays, the first one with all of the lower -case characters in the alphabet.  The second array will have all of
+            //the upper case letters in the alphabet.  If you have special characters in your name that are outside of the standard english alphabet,
+            //you can disregard this or you can add them to your arrays as well.Using your two arrays, access the correct elements to concatenate your name and output 
+            //it to the console window.Feel free to use the following code to get you started:
+
+
+
+
+        }
+    }
+}
